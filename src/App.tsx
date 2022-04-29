261178220be295b4c2df34411bfab0a268a50448
Victoria <vi.park6@gmail.com>
2022-04-29 16:52:08 +0200

    create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import { Navbar } from './pages/components/Navbar';
+import { Feed } from './pages/Feed/Feed';
+import { Login } from './pages/Login/Login';
+import { Signup } from './pages/Signup/Signup';
+import { CreatePost } from './pages/CreatePost/CreatePost';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Feed />} />
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<Signup />} />
+          <Route path='createpost' element={<CreatePost />} />
+          <Route path='*' element={
+            <p>Uh-oh! Page not found...</p>
+          } />
+        </Routes>
 
-    </div>
+    </BrowserRouter>
   );
 }
 
