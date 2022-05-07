20d56e5d367461624a85793080d9efc92ee2b0e2
Victoria <vi.park6@gmail.com>
2022-05-07 23:19:57 +0200

    hide CreateComponent if user is not authenticated

--- src/pages/PostSummary/PostSummary.tsx
+++ src/pages/PostSummary/PostSummary.tsx
@@ -3,12 +3,14 @@ import { useParams } from 'react-router-dom';
 import { CreateComment } from '../../components/CreateComment';
 import { Post } from '../../components/Post';
 import { PostComments } from '../../components/PostComments';
+import { useAuthContext } from '../../hooks/useAuthContext';
 import { useFirestoreDoc } from '../../hooks/useFirestoreDoc';
 
 export const PostSummary = () => {
   // struggled with TS2345: Argument of type 'string | undefined' is not assignable to parameter of type 'string'.
-
   const { id } = useParams<{ id?: string }>();
+
+  const { user } = useAuthContext();
   let { post, error } = useFirestoreDoc('posts', id);
 
   if (error) {
@@ -17,12 +19,13 @@ export const PostSummary = () => {
   if (!document) {
     return <div>Loading...</div>
   }
+
   return (
     <div>
       {post && (
         <>
           <Post post={post} showPostContent={true} />
-          <CreateComment post={post} />
+          {user && <CreateComment post={post} />}
           <PostComments comments={post.comments} />
         </>
       )}
